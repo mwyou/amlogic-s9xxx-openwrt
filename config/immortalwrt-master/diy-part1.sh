85af64fc03c61f702d441878de0a150cdef64be1
尤明旺 <2914406369@qq.com>
2025-10-18 10:08:45 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -17,9 +17,5 @@ set -e
 grep -q 'sundaqiang/openwrt-packages' feeds.conf.default || \
 echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages' >> feeds.conf.default
 
-# 拉取 luci-app-tailscale（Tailscale 的 LuCI 管理界面，来源 asvow 项目）
-rm -rf package/luci-app-tailscale
-git clone --depth=1 https://github.com/asvow/luci-app-tailscale.git package/luci-app-tailscale
-
 grep -q 'kenzok8/openwrt-packages' feeds.conf.default || \
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
